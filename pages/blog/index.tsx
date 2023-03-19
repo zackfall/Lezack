40483da2ee2730a27ab14502b93e12dce89c7f00
Isaac Zamora <zackfall@outlook.com>
2023-03-19 01:56:47 -0500

    feat: add a layout to the blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,20 @@
+import Layout from "@components/Layouts/GeneralLayout";
 import { NextPageWithLayout } from "@pages/_app";
+import { Roboto } from "next/font/google";
+import { ReactElement } from "react";
+
+const roboto = Roboto({
+	weight: "400",
+	subsets: ["latin"],
+	variable: "--font-roboto",
+});
 
 const Blog: NextPageWithLayout = () => {
-  return (
-    <>
-    </>
-  );
-}
+	return <></>;
+};
+
+Blog.getLayout = function getLayout(page: ReactElement) {
+	return <Layout className={`${roboto.variable} font-sans`}>{page}</Layout>;
+};
 
 export default Blog;
