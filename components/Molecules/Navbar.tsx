80ebd172a86e512946e181deec0b2bee00ce06f8
zackfall <zackfall@outlook.com>
2023-03-24 11:34:45 -0500

    style(Navbar): change navbar style for mobile devices

--- components/Molecules/Navbar.tsx
+++ components/Molecules/Navbar.tsx
@@ -12,9 +12,9 @@ export default function Navbar() {
 	};
 
 	return (
-		<nav className="flex justify-between lg:justify-start items-center lg:mx-20 mx-10 my-5">
+		<nav className="flex justify-between lg:justify-start flex-col lg:items-center lg:flex-row lg:mx-20 ml-8 my-5">
 			<h3 className="text-gray-800 text-xl font-medium">Lezack</h3>
-			<ul className="lg:flex lg:ml-16 space-x-14 justify-end hidden">
+			<ul className="flex lg:ml-16 lg:space-x-14 space-x-3 text-xs">
 				<li>
 					<Link
 						className="text-green-700 hover:text-orange-800 duration-200 text-lg"
@@ -32,57 +32,6 @@ export default function Navbar() {
 					</Link>
 				</li>
 			</ul>
-			<div className="relative lg:hidden h-12 w-12">
-				<button
-					className="flex flex-col absolute border-2 border-orange-800 rounded justify-center items-center group w-full h-full"
-					onClick={handleClick}
-				>
-					<div
-						className={`${hamburgerClass} ${
-							isOpen
-								? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
-								: "opacity-50 group-hover:opacity-100"
-						}`}
-					/>
-					<div
-						className={`${hamburgerClass} ${
-							isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
-						}`}
-					/>
-					<div
-						className={`${hamburgerClass} ${
-							isOpen
-								? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
-								: "opacity-50 group-hover:opacity-100"
-						}`}
-					/>
-				</button>
-				<div
-					className={`w-7 overflow-hidden rotate-180 absolute top-12 left-2.5 ${
-						isOpen ? "inline-block" : "hidden"
-					}`}
-				>
-					<div className="h-5 w-5 bg-orange-800 rotate-45 origin-bottom-left" />
-				</div>
-				<div
-					className={`w-52 rounded-lg bg-white h-32 absolute border-2 mx-0 my-auto -left-40 border-orange-800 top-16 ${
-						isOpen ? "block" : "hidden"
-					}`}
-				>
-					<ul className="flex flex-col justify-around w-full h-full justify-items-center">
-						<li className="pl-5 py-4.5">
-							<Link className="text-green-700 text-lg" href="/">
-								Home
-							</Link>
-						</li>
-						<li className="pl-5 py-4.5">
-							<Link className="text-green-700 text-lg" href="/blog">
-								Blog
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</div>
 		</nav>
 	);
 }
