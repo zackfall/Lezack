065cd4a1e81c6336fd79f3a3010921a25f00aead
Isaac Zamora <zackfall@outlook.com>
2023-04-08 12:02:22 -0500

    feat: make the links be a different color when active

--- components/Molecules/Navbar.tsx
+++ components/Molecules/Navbar.tsx
@@ -1,33 +1,23 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
-	const hamburgerClass =
-		"h-1 w-6 my-1 rounded-full bg-orange-800 transition ease transform duration-300";
-	const [isOpen, setIsOpen] = useState(false);
-	const handleClick = () => {
-		console.log(isOpen);
-		setIsOpen(!isOpen);
-		console.log(isOpen);
-	};
-
+	const router = useRouter();
+	const activeHome =
+		router.pathname === "/" ? "text-orange-800" : "text-green-700";
+	const activeBlog =
+		router.pathname === "/blog" ? "text-orange-800" : "text-green-700";
 	return (
 		<nav className="flex justify-between lg:justify-start flex-col lg:items-center lg:flex-row lg:mx-20 ml-8 my-5">
 			<h3 className="text-gray-800 text-xl font-medium">Lezack</h3>
 			<ul className="flex lg:ml-16 lg:space-x-14 space-x-3 text-xs">
 				<li>
-					<Link
-						className="text-green-700 hover:text-orange-800 duration-200 text-lg"
-						href="/"
-					>
+					<Link className={`${activeHome} duration-200 text-lg`} href="/">
 						Home
 					</Link>
 				</li>
 				<li>
-					<Link
-						className="text-green-700 hover:text-orange-800 duration-200 text-lg"
-						href="/blog"
-					>
+					<Link className={`${activeBlog} duration-200 text-lg`} href="/blog">
 						Blog
 					</Link>
 				</li>
