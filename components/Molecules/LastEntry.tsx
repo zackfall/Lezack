33531b26b1f12e5d2b63dfbd48747e43d9224a4f
Isaac Zamora <zackfall@outlook.com>
2023-07-26 22:14:16 -0500

    style: change size of the preview image

--- components/Molecules/LastEntry.tsx
+++ components/Molecules/LastEntry.tsx
@@ -10,17 +10,17 @@ interface LastEntryProps {
 export default function LastEntry({ post, tagColor }: LastEntryProps) {
   return (
     <section>
-      <h3 className="text-xl my-3">Last Post</h3>
-      <div className="relative border-gray-900 rounded-lg border-2">
+      <h3 className="text-xl my-5">Last Post</h3>
+      <div className="relative w-2/3 m-auto border-gray-900 rounded-lg border-2">
         <Image
-          className="blur-[2px] sm:blur-sm h-full w-full"
+          className="blur-[2px] sm:blur-sm w-full"
           src={post.frontMatter.image}
           alt="Prueba por ahora"
           fill={false}
         />
-        <div className="absolute bottom-0 left-0 bg-white bg-opacity-80 w-full h-28 border-2 border-black rounded-b-lg sm:h-48 lg:h-60 xl:h-80 2xl:h-96">
+        <div className="absolute bottom-0 left-0 bg-white bg-opacity-80 w-full h-28 border-2 border-black rounded-b-lg sm:h-36 lg:h-56 xl:h-64 2xl:h-80">
           <BlogPost
-            className="relative top-1/4 lg:top-[40%] w-64 left-9 sm:w-2/3 lg:w-[70%] trunc-smaller"
+            className="relative top-1/4 lg:top-[30%] 2xl:top-[35%] w-40 left-6 sm:w-10/12 lg:w-[85%] trunc-smaller"
             key={post.slug}
             isLast={true}
             tag={post.frontMatter.tag}
