284d141ea23dd6331159e992eadb2ea0901a84ec
Isaac Zamora <zackfall@outlook.com>
2023-07-26 22:41:18 -0500

    feat: create the Post component to render the posts

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -2,42 +2,47 @@ import { getFiles, getPostBySlug } from "lib/utils";
 import ReactMarkdown from "react-markdown";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { BlogPostProps } from "lib/types";
+import { Post } from "@components/Organisms/Post";
 
 export async function getStaticPaths() {
-	const posts = await getFiles("posts");
+  const posts = await getFiles("posts");
 
-	const paths = posts.map((filename: string) => ({
-		params: {
-			slug: filename.replace(".md", ""),
-		},
-	}));
+  const paths = posts.map((filename: string) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
 
-	return {
-		paths,
-		fallback: false,
-	};
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }: Params) {
-	const { frontMatter, markdownBody } = await getPostBySlug(
-		"posts",
-		params.slug,
-	);
+  const { frontMatter, markdownBody } = await getPostBySlug(
+    "posts",
+    params.slug
+  );
 
-	return {
-		props: {
-			frontMatter,
-			markdownBody,
-		},
-	};
+  return {
+    props: {
+      frontMatter,
+      markdownBody,
+    },
+  };
 }
 
-const BlogPost = ({ frontMatter, markdownBody }: BlogPostProps) => {
-	if (!frontMatter) return <></>;
+const BlogPost = (post: BlogPostProps) => {
+  if (!post.frontMatter) return <></>;
 
-	if (markdownBody.length < 1) return <>Empty</>;
+  if (post.markdownBody.length < 1) return <>Empty</>;
 
-	return <ReactMarkdown>{markdownBody}</ReactMarkdown>;
+  return (
+    <Post post={post}>
+      <ReactMarkdown>{post.markdownBody}</ReactMarkdown>
+    </Post>
+  );
 };
 
 export default BlogPost;
